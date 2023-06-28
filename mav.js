05f759b1ecd599f040ba7ecb905e074953c49950
badmen87 <98800441+badmen87@users.noreply.github.com>
2023-06-28 14:50:27 +0300

    fix

--- mav.js
+++ mav.js
@@ -20,25 +20,29 @@ async function main() {
     const wallets = await read('wallets.txt')
 
     for(let wallet of wallets) {
-        const response = await axios.get(`https://api.mav.xyz/api/claim/1/${wallet}`, {
-            headers: {
-                'authority': 'api.mav.xyz',
-                'accept': '*/*',
-                'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-                'cache-control': 'no-cache',
-                'origin': 'https://governance.mav.xyz',
-                'pragma': 'no-cache',
-                'referer': 'https://governance.mav.xyz/',
-                'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-site',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-            }
-        })
-        console.log(`${wallet} ${web3.utils.fromWei(response.data.claim.amount, 'ether')}`)
+        try {
+            const response = await axios.get(`https://api.mav.xyz/api/claim/1/${wallet}`, {
+                headers: {
+                    'authority': 'api.mav.xyz',
+                    'accept': '*/*',
+                    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                    'cache-control': 'no-cache',
+                    'origin': 'https://governance.mav.xyz',
+                    'pragma': 'no-cache',
+                    'referer': 'https://governance.mav.xyz/',
+                    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'same-site',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+                }
+            })            
+            console.log(`${wallet} ${web3.utils.fromWei(response.data.claim.amount, 'ether')}`)
+        } catch(e) {
+            console.log(`${wallet} 0`)
+        }
     }
 }
 
